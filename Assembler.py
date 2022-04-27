0184c23e19b3fcac6322949fa26eccad7f02864e
Pritika23 <pritika.hcb@gmail.com>
2022-04-27 07:00:06 +0530

    Updated

--- Assembler.py
+++ Assembler.py
@@ -1,6 +1,7 @@
 OPTAB = {"ADD": "18", "ADDR": "58", "CLEAR": "B4", "COMP": "28", "COMPR": "A0", "J": "3C", "JEQ": "30", "JLT": "38", "JSUB": "48", "LDA": "00", "LDB": "68",
          "LDCH": "50", "LDT": "74", "RD": "D8", "RSUB": "4C", "STA": "0C", "STCH": "54", "STL": "14", "STX": "10", "TD": "E0", "TIXR": "B8", "WD": "DC"}
 LOCCTR = '000000'
+BASE = ''
 ADCT = 0
 FORMAT2 = ["ADDR", "CLEAR", "COMPR", "TIXR"]
 KEYWORDS = ["BYTE", "RESW", "RESB", "WORD"]
@@ -106,16 +107,26 @@ def tohex(val, nbits):  # for converting negative hex numbers in FF.. format
     return hex((val + (1 << nbits)) % (1 << nbits))
 
 
-f3 = open("Intermediate.txt", "a+")  # make this only read mode!
-f3.seek(0)
+def calc_disp(TA, PC):
+    if TA-PC >= -2048 and TA-PC <= 2047:
+        return TA-PC
+    return TA-BASE
+
+
+f3 = open("Intermediate.txt", "r")
 f4 = open("Object_Program.txt", "w+")
-for line in f3:
+temp1 = f3.readlines()
+l = len(temp1)
+for i in range(l):
+    line = temp1[i]
     temp = line.split()
-    temp1 = f3.readline()  # readlines()
-    line1 = temp1.split()
+    # BASE directive
+    if temp[-2] == 'BASE':
+        BASE = int(SYMTAB[temp[-1]], 16)
     if temp[-2] != "END":
-        PC = int(line1[0], 16)
-        print(line1[0])
+        line1 = temp1[i+1]
+        l1 = line1.split()
+        PC = int(l1[0], 16)
         # header
         if temp[-2] == "START":
             f4.write("H^"+PROGNAME+"^"+"000000^"+LOCCTR)
@@ -123,12 +134,10 @@ for line in f3:
         # Format 4 instructions without immediate addressing
         elif temp[-2].replace('+', '') in OPTAB and '+' in temp[-2] and '#' not in temp[-1]:
             t = temp[-2].replace('+', '')
-            OPCODE = OPTAB[t]
-            i2 = int(OPCODE[1])
+            OPCODE = OPTAB[t.replace('\n', '')]
+            i2 = int(OPCODE[1], 16)
             b1 = bin(i2)
-            l1 = list(b1)
-            l1[-1] = l1[-2] = '1'
-            b1 = "".join(l1)
+            b1 = b1[:-2]+'11'
             b1 = b1.replace('0b', '')
             i2 = int(b1, 2)
             h1 = hex(i2)
@@ -140,12 +149,10 @@ for line in f3:
         # Format 4 instructions with immediate addressing
         elif temp[-2].replace('+', '') in OPTAB and '+' in temp[-2] and '#' in temp[-1]:
             t = temp[-2].replace('+', '')
-            OPCODE = OPTAB[t]
-            i2 = int(OPCODE[1])
+            OPCODE = OPTAB[t.replace('\n', '')]
+            i2 = int(OPCODE[1], 16)
             b1 = bin(i2)
-            l1 = list(b1)  # s[:-2]+'11'
-            l1[-1] = l1[-2] = '1'
-            b1 = "".join(l1)
+            b1 = b1[:-2]+'11'
             b1 = b1.replace('0b', '')
             i2 = int(b1, 2)
             h1 = hex(i2)
@@ -153,7 +160,7 @@ for line in f3:
             obj_code = OPCODE[0] + h1.upper() + '1'
             if temp[-1].replace('#', '') in SYMTAB:
                 TA = SYMTAB[temp[-1].replace('#', '')]
-                TA = TA.zfill(5)
+                TA = str(TA).zfill(5)
                 obj_code += TA[-5]+TA[-4]+TA[-3]+TA[-2]+TA[-1]
             elif temp[-1].replace('#', '') not in SYMTAB:
                 TA = temp[-1].replace('#', '')
@@ -162,35 +169,28 @@ for line in f3:
                 f4.write('\n'+line+"    "+obj_code.ljust(4)+"    ")
 
         # normal format 3 instructions AND indirect: not F4, not F2, not indexed, not a declaration, not immediate, not indirect
-        if temp[-2] in OPTAB and '+' not in temp[-2] and temp[-2] not in FORMAT2 and ',X' not in temp[-1] and temp[-2] not in KEYWORDS and '#' not in temp[-1] and '@' not in temp[-1]:
-            OPCODE = OPTAB[temp[-2]]
-            i2 = int(OPCODE[1])
+        elif temp[-2] in OPTAB and '+' not in temp[-2] and temp[-2] not in FORMAT2 and ',X' not in temp[-1] and temp[-2] not in KEYWORDS and '#' not in temp[-1] and '@' not in temp[-1]:
+            OPCODE = OPTAB[temp[-2].replace('\n', '')]
+            i2 = int(OPCODE[1], 16)
             b1 = bin(i2)
-            l1 = list(b1)
-            l1[-1] = l1[-2] = '1'
-            b1 = "".join(l1)
+            b1 = b1[:-2]+'11'
             b1 = b1.replace('0b', '')
             i2 = int(b1, 2)
             h1 = hex(i2)
             h1 = h1.replace('0x', '')
             obj_code = OPCODE[0] + h1.upper() + '2'
             TA = int(SYMTAB[temp[-1]], 16)
-            disp = TA-PC
+            disp = calc_disp(TA, PC)
             disps = tohex(disp, 12)
             disps = disps.replace('0x', '')
             obj_code += (disps.upper()).zfill(3)
             f4.write('\n'+line+"    "+obj_code.ljust(4)+"    ")
         # indirect addressing in F3 -> doesn't work
-        if '@' in temp[-1] and temp[-2] in OPTAB and '+' not in temp[-2] and temp[-2] not in FORMAT2 and ',X' not in temp[-1] and temp[-2] not in KEYWORDS and '#' not in temp[-1]:
-            print(temp[-1])
-            OPCODE = OPTAB[temp[-2]]
-            i2 = int(OPCODE[1])
+        elif '@' in temp[-1] and temp[-2] in OPTAB and '+' not in temp[-2] and temp[-2] not in FORMAT2 and ',X' not in temp[-1] and temp[-2] not in KEYWORDS and '#' not in temp[-1]:
+            OPCODE = OPTAB[temp[-2].replace('\n', '')]
+            i2 = int(OPCODE[1], 16)
             b1 = bin(i2)
-            l1 = list(b1)
-            # this is test
-            l1[-1] = '0'
-            l1[-2] = '1'
-            b1 = "".join(l1)
+            b1 = b1[:-2]+'10'
             b1 = b1.replace('0b', '')
             i2 = int(b1, 2)
             h1 = hex(i2)
@@ -198,21 +198,18 @@ for line in f3:
             obj_code = OPCODE[0] + h1.upper() + '2'
             o = temp[-1].replace('@', '')
             TA = int(SYMTAB[o], 16)
-            disp = TA-PC
+            disp = calc_disp(TA, PC)
             disps = tohex(disp, 12)
             disps = disps.replace('0x', '')
             obj_code += (disps.upper()).zfill(3)
             f4.write('\n'+line+"    "+obj_code.ljust(4)+"    ")
         # immediate addressing in F3
-        if '#' in temp[-1] and '+' not in temp[-2] and temp[-2] not in FORMAT2 and ',X' not in temp[-1] and temp[-2] not in KEYWORDS and '@' not in temp[-1]:
+        elif '#' in temp[-1] and '+' not in temp[-2] and temp[-2] not in FORMAT2 and ',X' not in temp[-1] and temp[-2] not in KEYWORDS and '@' not in temp[-1]:
             o = temp[-1].replace('#', '')
-            OPCODE = OPTAB[temp[-2]]
-            i2 = int(OPCODE[1])
+            OPCODE = OPTAB[temp[-2].replace('\n', '')]
+            i2 = int(OPCODE[1], 16)
             b1 = bin(i2)
-            l1 = list(b1)
-            l1[-1] = '1'
-            l1[-2] = '0'
-            b1 = "".join(l1)
+            b1 = b1[:-2]+'01'
             b1 = b1.replace('0b', '')
             i2 = int(b1, 2)
             h1 = hex(i2)
@@ -225,4 +222,33 @@ for line in f3:
                 disps = tohex(val, 12)
             disps = disps.replace('0x', '')
             obj_code += (disps.upper()).zfill(3)
+            f4.write('\n'+line+"    "+obj_code.ljust(4)+"    ")
+        # indexed addressing  -> hard coded for BUFFER, X
+        elif ',X' in temp[-1]:
+            OPCODE = OPTAB[temp[-2].replace('\n', '')]
+            i2 = int(OPCODE[1], 16)
+            b1 = bin(i2)
+            b1 = b1[:-2]+'11'
+            b1 = b1.replace('0b', '')
+            i2 = int(b1, 2)
+            h1 = hex(i2)
+            h1 = h1.replace('0x', '')
+            obj_code = OPCODE[0] + h1.upper() + 'C'
+            TA = int(SYMTAB[(temp[-1])[:-2]], 16)
+            disp = calc_disp(TA, PC)
+            disps = tohex(disp, 12)
+            disps = disps.replace('0x', '')
+            obj_code += (disps.upper()).zfill(3)
+            #print('\n'+line+"    "+obj_code.ljust(4)+"    ")
+            f4.write('\n'+line+"    "+obj_code.ljust(4)+"    ")
+        # format 2
+        elif temp[-2] in FORMAT2:
+            OPCODE = OPTAB[temp[-2].replace('\n', '')]
+            if ',' in temp[-1]:
+                r1 = REGISTERS[temp[-1][0]]
+                r2 = REGISTERS[temp[-1][2]]
+                obj_code = OPCODE+r1+r2
+            else:
+                r = REGISTERS[temp[-1]]
+                obj_code = OPCODE+r+'0'
             f4.write('\n'+line+"    "+obj_code.ljust(4)+"    ")
